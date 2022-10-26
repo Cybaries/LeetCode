c3859de724d71da9b23c97277453b954996f45b6
Cybaries <65446412+Cybaries@users.noreply.github.com>
2022-10-26 19:23:03 +0530

    Time: 47 ms (92.65%), Space: 5.7 MB (89.66%) - LeetHub

--- 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.cpp
+++ 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.cpp
@@ -1,18 +1,19 @@
-class Solution {
-    int reverse(int n){
-        int res = 0;
-        while(n){
-            res = res*10 + (n%10);
-            n /= 10;
+class Solution
+{
+    public:
+        bool sumOfNumberAndReverse(int num)
+        {
+            for (int i = num / 2; i <= num; i++)
+            {
+                int res = 0, n = i;
+                while (n)
+                {
+                    res = res *10 + (n % 10);
+                    n /= 10;
+                }
+                if (i + res == num)
+                    return true;
+            }
+            return false;
         }
-        return res;
-    }
-public:
-    bool sumOfNumberAndReverse(int num) {
-        for(int i = num/2; i <= num; i++){
-            if (i + reverse(i) == num)
-                return true;
-        }
-        return false;
-    }
 };

2ee4e278c802f4d816083efe06595985525fecda
Cybaries <65446412+Cybaries@users.noreply.github.com>
2022-12-16 13:40:23 +0530

    Time: 7 ms (90.58%), Space: 13.2 MB (38.61%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,38 +1,49 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-       queue<vector<int>> q;
-        vector<vector<int>> v {{1,0},{-1,0},{0,1},{0,-1}};
-        int rows = grid.size();
-        int cols = grid[0].size();
-        int healthy = 0;
-        for (int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if (grid[i][j] == 2)
-                    q.push({i, j});
-                if (grid[i][j] == 1)
-                    healthy++;
-            }
-        }
-        int minutes = 0;
-        while(healthy>0 && !q.empty()){
-            minutes++;
-            int lim = q.size();
-            for (int i = 0; i < lim; i++){
-                for (auto j: v){
-                    vector<int> k = q.front();
-                    if(k[0]+j[0] < 0 || k[0]+j[0] >= rows || k[1]+j[1] < 0 || k[1]+j[1] >= cols)
-                        continue;
-                    if(grid[k[0]+j[0]][k[1]+j[1]] ==2 || grid[k[0]+j[0]][k[1]+j[1]] == 0)
-                        continue;
-                    // cout<<k[0]<<" "<<j[0]<<" "<<k[1]<<" "<<j[1]<<endl;
-                    healthy--;
-                    grid[k[0]+j[0]][k[1]+j[1]] = 2;
-                    q.push({k[0]+j[0], k[1]+j[1]});
+      int n = grid.size();
+        int m = grid[0].size();
+        // n*m 
+        // {{r, c}, t}
+        queue<pair< pair<int, int>, int>> q;
+        // n*m 
+        int vis[n][m];
+        int cntFresh = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == 2){
+                    q.push({{i, j}, 0});
+                    vis[i][j] = 2;
+                }else{
+                    vis[i][j] = 0;
                 }
-                q.pop();
+                if(grid[i][j]==1) cntFresh++;
             }
         }
-        return healthy == 0? minutes:-1;
+
+        int tm = 0;
+        int drow[] = {-1, 0, +1, 0};
+        int dcol[] = {0, 1, 0, -1};
+        int cnt = 0;
+        while(!q.empty()){
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int t = q.front().second;
+            tm = max(tm, t);
+            q.pop();
+            for(int i=0; i<4; i++){
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m
+                && vis[nrow][ncol]==0 && grid[nrow][ncol] == 1){
+                    q.push({{nrow, ncol}, t+1});
+                    vis[nrow][ncol] = 2;
+                    cnt++;
+                }
+            }
+        }
+        if(cnt != cntFresh) return -1;
+
+        return tm;
     }
 };

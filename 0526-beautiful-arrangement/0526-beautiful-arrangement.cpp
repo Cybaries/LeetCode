9823292a1dfbcb6045e260483849314a8e3eb7e9
Cybaries <65446412+Cybaries@users.noreply.github.com>
2023-01-14 14:02:21 +0530

    Time: 451 ms (23.31%), Space: 135.4 MB (9.17%) - LeetHub

---
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    void dj(vector <int> v, int index, int &ans){
+        if(index==v.size()){
+            ans++;
+            return;
+        }
+        for(int i=index;i<v.size();i++){
+            if(v[i]%(index+1)==0 || (index+1)%v[i]==0){
+                swap(v[i], v[index]);
+                dj(v, index+1, ans);
+                swap(v[i], v[index]);
+            }
+        }
+    }
+public:
+    int countArrangement(int n) {
+        vector <int> v;
+        int ans=0;
+        for(int i=1; i <= n; i++){
+            v.push_back(i);
+        }
+        dj(v, 0, ans);
+        return ans;
+    }
+};

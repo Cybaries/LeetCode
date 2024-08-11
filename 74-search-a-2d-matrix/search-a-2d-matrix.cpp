51611f8421f4f1b3942986aab113cee37ec27d43
Aman Jha <65446412+Cybaries@users.noreply.github.com>
2024-08-11 19:47:06 +0530

    Time: 3 ms (66.29%) | Memory: 12.2 MB (21.67%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size(), m = matrix[0].size();
+        
+        // Binary search to find the correct row
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (matrix[mid][0] <= target && target <= matrix[mid][m - 1]) {
+                // Binary search in the found row
+                int l = 0, r = m - 1;
+                while (l <= r) {
+                    int midCol = (l + r) / 2;
+                    if (matrix[mid][midCol] == target) {
+                        return true;
+                    } else if (matrix[mid][midCol] < target) {
+                        l = midCol + 1;
+                    } else {
+                        r = midCol - 1;
+                    }
+                }
+                return false;  // If not found in the row
+            } else if (matrix[mid][0] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;  // If the row was not found
+    }
+};

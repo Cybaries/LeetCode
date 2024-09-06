2ac263aa5f7ac598d50179daccfd42925938bf77
Aman Jha <65446412+Cybaries@users.noreply.github.com>
2024-09-06 23:37:58 +0530

    Time: 6 ms (56.94%) | Memory: 15.2 MB (29.83%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -15,94 +15,22 @@ public:
 */
 
 class Solution {
-private:
-    // Function to insert a copy of each
-    // node in between the original nodes
-    void insertCopyInBetween(Node* head){
-        Node* temp = head;
-        while(temp != NULL){
-            Node* nextElement = temp->next;
-            // Create a new node with the same val
-            Node* copy = new Node(temp->val);  
-            
-            // Point the copy's next to
-            // the original node's next
-            copy->next = nextElement;  
-            
-            // Point the original
-            // node's next to the copy
-            temp->next = copy;         
-
-            // Move to the next original node
-            temp = nextElement;         
-        }
-    }
-
-    // Function to connect random
-    // pointers of the copied nodes
-    void connectRandomPointers(Node* head){
-        Node* temp = head;
-        while(temp != NULL){
-            // Access the copied node
-            Node* copyNode = temp->next;    
-            
-            // If the original node
-            // has a random pointer
-            if(temp->random){   
-                // Point the copied node's random to the
-                // corresponding copied random node
-                copyNode->random = temp->random->next; 
-            }
-            else{
-                // Set the copied node's random to
-                // null if the original random is null
-                copyNode->random = NULL;   
-            }
-            
-            // Move to the next original node
-            temp = temp->next->next;   
-        }
-    }
-
-    // Function to retrieve the
-    // deep copy of the linked list
-    Node* getDeepCopyList(Node* head){
-        Node* temp = head;
-        // Create a dummy node
-        Node* dummyNode = new Node(-1);   
-        // Initialize a result pointer
-        Node* res = dummyNode;             
-
-        while(temp != NULL){
-            // Creating a new List by
-            // pointing to copied nodes
-            res->next = temp->next;
-            res = res->next;
-
-            // Disconnect and revert back to the
-            // initial state of the original linked list
-            temp->next = temp->next->next;
-            temp = temp->next;
-        }
-        
-        // Return the deep copy of the
-        // list starting from the dummy node
-        return dummyNode->next;   
-    }
 public:
     Node* copyRandomList(Node* head) {
-        // If the original list
-        // is empty, return null
-        if(!head) return nullptr;   
-        
-        // Step 1: Insert copy of
-        // nodes in between
-        insertCopyInBetween(head); 
-        // Step 2: Connect random
-        // pointers of copied nodes
-        connectRandomPointers(head);  
-        // Step 3: Retrieve the deep
-        // copy of the linked list
-        return getDeepCopyList(head); 
+        Node* temp=head;
+        unordered_map<Node*, Node*>mp;
+        while(temp!=NULL){
+            Node* n=new Node(temp->val);
+            mp[temp]=n;
+            temp=temp->next;
+        }
+        temp=head;
+        while(temp!=NULL){
+            Node* copy=mp[temp];
+            copy->next=mp[temp->next];
+            copy->random=mp[temp->random];
+            temp=temp->next;
+        }
+        return mp[head];
     }
 };

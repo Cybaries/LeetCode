0e004584623546a8b14d95f306ee355f9b8c2216
Aman Jha <65446412+Cybaries@users.noreply.github.com>
2025-09-16 20:00:01 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (86.67%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-
-    int majorityElement(vector<int> v) {
-
-        //size of the given array:
-        int n = v.size();
-        int cnt = 0; // count
-        int el; // Element
-
-        //applying the algorithm:
-        for (int i = 0; i < n; i++) {
-            if (cnt == 0) {
-                cnt = 1;
-                el = v[i];
+    int majorityElement(vector<int>& nums) {
+        int cnt = 0;
+        int element = nums[0];
+        for (int i: nums){
+            if (cnt == 0 || element == i){
+                element = i;
+                cnt++;
             }
-            else if (el == v[i]) cnt++;
-            else cnt--;
+            else{
+                cnt--;
+            }
         }
+        return element;
 
-        //checking if the stored element
-        // is the majority element:
-        int cnt1 = 0;
-        for (int i = 0; i < n; i++) {
-            if (v[i] == el) cnt1++;
-        }
-
-        if (cnt1 > (n / 2)) return el;
-        return -1;
     }
 };

c041cd8896e037b965f72181064cfcc9e72720a7
Aman Jha <65446412+Cybaries@users.noreply.github.com>
2025-09-16 19:55:07 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (76.34%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,46 +1,33 @@
-class Solution
-{
-    public:
-        bool searchMatrix(vector<vector < int>> &matrix, int target)
-        {
-            int n = matrix.size(), m = matrix[0].size();
-
-           	// Binary search to find the correct row
-            int low = 0, high = n - 1;
-            while (low <= high)
-            {
-                int mid = (low + high) / 2;
-                if (matrix[mid][0] <= target && target <= matrix[mid][m - 1])
-                {
-                   	// Binary search in the found row
-                    int l = 0, r = m - 1;
-                    while (l <= r)
-                    {
-                        int midCol = (l + r) / 2;
-                        if (matrix[mid][midCol] == target)
-                        {
-                            return true;
-                        }
-                        else if (matrix[mid][midCol] < target)
-                        {
-                            l = midCol + 1;
-                        }
-                        else
-                        {
-                            r = midCol - 1;
-                        }
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size(), m = matrix[0].size();
+        int low = 0, high = n-1;
+        while (low <= high){
+            int mid = (low + high)/2;
+            if (target >= matrix[mid][0] && target <= matrix[mid][m-1]){
+                int l = 0, h = m-1;
+                while (l <= h){
+                    int midRow = (l+h)/2;
+                    if (matrix[mid][midRow] == target){
+                        return true;
                     }
-                    return false;	// If not found in the row
+                    else if ( target < matrix[mid][midRow]){
+                        h = midRow - 1;
+                    }
+                    else{
+                        l = midRow + 1;
+                    }
                 }
-                else if (matrix[mid][0] > target)
-                {
-                    high = mid - 1;
-                }
-                else
-                {
-                    low = mid + 1;
-                }
+                return false;
             }
-            return false;	// If the row was not found
+            else if (target < matrix[mid][0]){
+                high = mid -1;
+            }
+            else{
+                low = mid + 1;
+            }
         }
+        return false;
+    }
 };

1e4ec056faaea19c5b8b1ee5f462670871bb3719
Aman Jha <65446412+Cybaries@users.noreply.github.com>
2025-09-21 18:35:00 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (90.82%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void dfs(int index, vector<int>& candidates,int target, vector<int>& path, vector<vector<int>>& res){
+        if (target < 0 || index == candidates.size()) return;
+        if (target == 0){
+            res.push_back(path);
+            return;
+        }
+        //Include same
+        path.push_back(candidates[index]);
+        dfs (index, candidates, target - candidates[index], path, res);
+        path.pop_back();
+        //Exclude same + duplicates
+        while (index < candidates.size()-1 && candidates[index] == candidates[index+1]) index++;
+        dfs (index+1, candidates, target, path, res);
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<int> path;
+        vector<vector<int>> res;
+        dfs(0, candidates, target, path, res);
+        return res;
+    }
+};

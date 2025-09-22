5cea7026624775f155746524abe9b03f7928beb8
Aman Jha <65446412+Cybaries@users.noreply.github.com>
2025-09-23 01:34:12 +0530

    Time: 3 ms (27.45%) | Memory: 10.6 MB (47.06%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    void dfs(int index, vector<int> temp, vector<int>& nums, vector<vector<int>>& res){
-        if (index == nums.size()){
-            res.push_back(temp);
-            return;
+    void solve (vector<int>& nums,vector<vector<int>>&ans,int index){
+        if(index>=nums.size()){
+            ans.push_back(nums);
+            return ;
         }
-        for (int i = 0; i < nums.size(); i++){
-            if (find(temp.begin(), temp.end(), nums[i]) != temp.end()) continue; // Skip element if already in temp
-            //Include element
-            temp.push_back(nums[i]);
-            dfs(index+1, temp, nums, res);
-            temp.pop_back();
+        for(int j=index;j<nums.size();j++){
+            swap(nums[index],nums[j]);
+            solve(nums,ans,index+1);
+            swap(nums[index],nums[j]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> res;
-        vector<int> temp;
-        dfs(0, temp, nums, res);
-        return res;
+        vector<int>temp;
+        vector<vector<int>>ans;
+        solve(nums,ans,0);
+        return ans;
     }
 };

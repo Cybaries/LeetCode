4a0b6e6ba2d457deada3125a09b5fa1883bad943
Aman Jha <65446412+Cybaries@users.noreply.github.com>
2025-10-12 11:41:31 +0530

    Time: 131 ms (38.39%) | Memory: 53.7 MB (13.61%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
-    int dfs (int idx, int sum, int cnt, char currOper, vector<int>& nums, int target){
-        if (idx >=nums.size()){
-            if (sum == target){
-                cnt++;
-                return cnt;
-            }
-            else{
-                return cnt;
-            }
-        }
-        if (currOper == '+') sum += nums[idx];
-        if (currOper == '-') sum -= nums[idx];
-
-        return dfs(idx+1, sum, cnt, '+', nums, target) + dfs(idx+1, sum,cnt, '-', nums, target);
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int cnt =  dfs(0, 0, 0, '+', nums, target) + dfs(0, 0, 0, '-', nums, target);
-        return cnt/2;
+        unordered_map<int, int> counter;
+        counter[0] = 1;
+
+        for (int n : nums) {
+            unordered_map<int, int> temp;
+
+            for (const auto& [total, count] : counter) {
+                temp[total + n] += count;
+                temp[total - n] += count;
+            }
+            counter = temp;
+        }
+
+        return counter[target];
     }
 };

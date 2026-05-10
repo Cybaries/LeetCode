671c0f5816ad6d34a65625ebd0ff2cf42cc464b8
Aman Jha <65446412+Cybaries@users.noreply.github.com>
2026-05-11 01:33:04 +0530

    Create 232-ImplementQueueUsingStacks

---
+++ 232-ImplementQueueUsingStacks/232-ImplementQueueUsingStacks.cpp
@@ -0,0 +1,37 @@
+// Last updated: 11/05/2026, 01:33:04
+class MyQueue {
+    vector<int> s;
+    int cnts = 0;
+public:
+    MyQueue() {
+        s.resize(9);
+    }
+    
+    void push(int x) {
+        s[cnts++] = x;
+    }
+    
+    int pop() {
+        int x = s[0];
+        s.erase(s.begin());
+        cnts--;
+        return x;
+    }
+    
+    int peek() {
+        return s[0];
+    }
+    
+    bool empty() {
+        return !cnts;
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */

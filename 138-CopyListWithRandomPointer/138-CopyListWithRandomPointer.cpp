78d30fd31026350462c23325ae0197c147c57ebe
Aman Jha <65446412+Cybaries@users.noreply.github.com>
2026-05-11 01:33:33 +0530

    Create 138-CopyListWithRandomPointer

---
+++ 138-CopyListWithRandomPointer/138-CopyListWithRandomPointer.cpp
@@ -0,0 +1,37 @@
+// Last updated: 11/05/2026, 01:33:32
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        Node* temp=head;
+        unordered_map<Node*, Node*>mp;
+        while(temp!=NULL){
+            Node* n=new Node(temp->val);
+            mp[temp]=n;
+            temp=temp->next;
+        }
+        temp=head;
+        while(temp!=NULL){
+            Node* copy=mp[temp];
+            copy->next=mp[temp->next];
+            copy->random=mp[temp->random];
+            temp=temp->next;
+        }
+        return mp[head];
+    }
+};

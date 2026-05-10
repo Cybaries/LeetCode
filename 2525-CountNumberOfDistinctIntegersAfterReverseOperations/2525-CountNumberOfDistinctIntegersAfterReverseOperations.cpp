43f2280c8c3a40c56c5e5fddbd8eaf2370d4a6e0
Aman Jha <65446412+Cybaries@users.noreply.github.com>
2026-05-11 01:30:27 +0530

    Create 2525-CountNumberOfDistinctIntegersAfterReverseOperations

---
+++ 2525-CountNumberOfDistinctIntegersAfterReverseOperations/2525-CountNumberOfDistinctIntegersAfterReverseOperations.cpp
@@ -0,0 +1,19 @@
+// Last updated: 11/05/2026, 01:30:26
+class Solution {
+    int reverse(int n){
+        int res = 0;
+        while(n > 0){
+            res = res *10 + (n%10);
+            n /= 10;
+        }
+        return res;
+    }
+public:
+    int countDistinctIntegers(vector<int>& nums) {
+        unordered_set<int> st(begin(nums), end(nums));
+        for (int i = 0; i < nums.size(); i++){
+            st.insert(reverse(nums[i]));
+        }
+        return st.size();
+    }
+};

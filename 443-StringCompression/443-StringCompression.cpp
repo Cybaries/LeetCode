37ce337f1ab1253b5cd0f4992957f4ec129e2ee7
Aman Jha <65446412+Cybaries@users.noreply.github.com>
2026-05-11 01:32:40 +0530

    Create 443-StringCompression

---
+++ 443-StringCompression/443-StringCompression.cpp
@@ -0,0 +1,20 @@
+// Last updated: 11/05/2026, 01:32:40
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int indexAns = 0, index = 0;
+        while(index < chars.size()){
+            char currentChar = chars[index];
+            int count = 0;
+            while(index < chars.size() && chars[index] == currentChar){
+                index++;
+                count++;
+            }
+            chars[indexAns++] = currentChar;
+            if(count != 1)
+                for(char c : to_string(count)) 
+                    chars[indexAns++] = c;
+        }
+        return indexAns;
+    }
+};

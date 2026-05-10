d19392ab24124c54e78e9febd92333c183d627f5
Aman Jha <65446412+Cybaries@users.noreply.github.com>
2026-05-11 01:32:13 +0530

    Create 795-KThSymbolInGrammar

---
+++ 795-KThSymbolInGrammar/795-KThSymbolInGrammar.cpp
@@ -0,0 +1,12 @@
+// Last updated: 11/05/2026, 01:32:13
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        if (n == 1 && k == 1) return 0;
+        int mid = pow(2,n-1)/2;
+        if (k <= mid)
+            return kthGrammar(n-1,k);
+        else
+            return !kthGrammar(n-1, k-mid);
+    }
+};

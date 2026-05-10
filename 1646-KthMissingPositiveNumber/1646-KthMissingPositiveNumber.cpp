2036078f6cea8c2b3fc9e9ad63d5fa2703bf5d21
Aman Jha <65446412+Cybaries@users.noreply.github.com>
2026-05-11 01:31:43 +0530

    Create 1646-KthMissingPositiveNumber

---
+++ 1646-KthMissingPositiveNumber/1646-KthMissingPositiveNumber.cpp
@@ -0,0 +1,16 @@
+// Last updated: 11/05/2026, 01:31:43
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+         int n = arr.size();
+        int start = 0, end = n-1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] - (mid + 1) < k)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return start + k;
+    }
+};

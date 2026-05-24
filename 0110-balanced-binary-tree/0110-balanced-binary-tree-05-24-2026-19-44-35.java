1008f7a8af720973accaf9a9148e0efb280eda14
Aman Jha <65446412+Cybaries@users.noreply.github.com>
2026-05-24 19:44:37 +0530

    Time - 0 ms (100%), Space - 45.6 MB (58.03%) 05-24-2026     Tree

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree-05-24-2026-19-44-35.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private boolean flag = true;
+    public boolean isBalanced(TreeNode root) {
+        dfs(root);
+        return flag;
+    }
+
+    private int dfs(TreeNode node){
+        if (node == null) return 0;
+
+        int leftH = dfs(node.left);
+        // if (leftH == -1) return -1;
+        int rightH  = dfs(node.right);
+        // if (rightH == -1) return -1;
+
+        if (Math.abs(leftH - rightH)>1){ 
+            flag = false;
+            return -1;
+        }
+
+        return 1 + Math.max(leftH, rightH);
+    }
+}
